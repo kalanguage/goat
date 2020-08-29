0214cd2e109cd51d0556bae7e31b702ec2ddf877
Ankizle <ankizleyt@gmail.com>
2020-08-29 00:53:34 -0400

    removed omm specific functions, and renamed GetOat

--- compile.go
+++ compile.go
@@ -1,28 +1,12 @@
 package goat
 
 import (
-	"io/ioutil"
-
-	"github.com/omm-lang/omm/lang/compiler"
 	oatenc "github.com/omm-lang/omm/oat/encoding"
 
 	. "github.com/omm-lang/omm/lang/types"
 )
 
-//Compile a Omm file
-func CompileFile(filename string) (map[string][]Action, compiler.CompileErr) {
-	f, _ := ioutil.ReadFile(filename)
-	vars, e := compiler.Compile(string(f), "goat compile", false, true)
-	return vars, e
-}
-
-//Compile an Omm string
-func CompileString(script string) (map[string][]Action, compiler.CompileErr) {
-	vars, e := compiler.Compile(script, "goat compile", false, true)
-	return vars, e
-}
-
-//Decompile an Oat file and store it in a map[string][]types.Action.
-func GetOat(file string) (map[string][]Action, error) {
+//LoadLibrary decompiles an Oat file and store it in a map[string][]types.Action.
+func LoadLibrary(file string) (map[string][]Action, error) {
 	return oatenc.OatDecode(file, 0)
 }
